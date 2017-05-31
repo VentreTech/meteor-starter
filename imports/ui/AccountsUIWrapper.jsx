cbb75632bbdc7622f1c84a7844bb9f3aea99c852
Thomas <tmcothran@hotmail.com>
2017-05-30 20:00:32 -0400

    Adds sass/scss support.

--- imports/ui/AccountsUIWrapper.jsx
+++ imports/ui/AccountsUIWrapper.jsx
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import { Template } from 'meteor/templating';
 import Blaze from 'meteor/gadicc:blaze-react-component';
 
 export default class AccountsUIWrapper extends Component {
